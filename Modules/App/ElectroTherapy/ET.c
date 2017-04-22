35f6d45549f46f6ba87eb63d93a196b41c31acb6
Juaninmejia <juaninmejia@outlook.com>
2017-04-22 00:51:49 -0500

    created functions for ET
    the input of the functions is an uint8 variable, this variable is the
    percentage provided by HMI which has a range from 0 to 100

--- Modules/App/ElectroTherapy/ET.c
+++ Modules/App/ElectroTherapy/ET.c
@@ -7,3 +7,30 @@
  * 	filename: ET.c
  */
 
+#include "ET.h"
+
+
+void ET_UpdateFrequency ( uint8 u8RawFrequency )
+{
+	float fFreqRange =	(MaxFrequency - MinFrequency);
+	float fFreqResolution = (fFreqRange/100);
+	uint8 u8Frequency = (MinFrequency + (u8RawFrequency * fFreqResolution));
+	if ((u8Frequency <= MaxFrequency) && (u8Frequency >= MinFrequency))
+	{
+		TEST_vChangingFreq ( u8Frequency );
+	}
+}
+
+void ET_UpdatePWM ( uint8 u8RawPWM )
+{
+	TPM_tenModules enMod = enTPM1;
+	TPM_tenChannels enChan = enChannel0;
+	float fPWMRange = (MaxPWM - MinPWM);
+	float fPWMResolution = (fPWMRange/(100));
+	uint8 u8PWM = (MinPWM + (u8RawPWM * fPWMResolution));
+			
+	if ((u8PWM <= MaxPWM )&& (u8PWM >= MinPWM))
+	{
+		TPM_vChangeDutyCycle(enMod, enChan, u8PWM);
+	}
+}
--- Modules/App/ElectroTherapy/ET.h
+++ Modules/App/ElectroTherapy/ET.h
@@ -10,6 +10,28 @@
 #ifndef ET_H_
 #define ET_H_
 
+#include "Test.h"
+#include "Debouncer.h"
+#include "TPM.h"
+
+#define KeyPressed			1
+
+#define MaxFrequency		100
+#define MinFrequency		20
+
+#define MaxPWM				100
+#define MinPWM				0
+
+//#define FreqRange		(MaxFrequency - MinFrequency)
+//#define FreqResolution	((FreqRange)/(100))
+//#define PWMResolution	((MaxPWM - MinPWM)/(100))
+
 void ET_Dumy ( void );
+
+void ET_UpdateFrequency ( uint8 u8Frequency );
+void ET_UpdatePWM ( uint8 u8PWM );
+
+extern void TPM_vChangeDutyCycle(TPM_tenModules enModule, TPM_tenChannels enChannel, uint8 u8DutyCycle);
+extern void TPM_vChangeFrecuency(TPM_tenModules enModule, TPM_tenChannels enChannel, uint8 u8Frecuency);
 
 #endif /* ET_H_ */
